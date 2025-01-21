c24d6af2312837a61b354ce5b7de17337a8da941
Informax <kirichenko.maxim@gmail.com>
2025-01-21 09:51:32 +0200

    Fix AutiInitialized::turnInto()

--- src/Classes/AutoInitialized.php
+++ src/Classes/AutoInitialized.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 /**
  * This file is part of microGears\Stdlib.
  *
@@ -12,8 +12,9 @@ declare(strict_types=1);
 
 namespace WebStone\Stdlib\Classes;
 
-use WebStone\Stdlib\Helpers\StringHelper;
+use ReflectionClass;
 use RuntimeException;
+use WebStone\Stdlib\Helpers\StringHelper;
 
 /**
  * AutoInitialized
@@ -21,38 +22,56 @@ use RuntimeException;
  * @author Maxim Kirichenko <kirichenko.maxim@gmail.com>
  * @datetime 07.05.2024 11:20:55
  */
-class AutoInitialized
-{
+class AutoInitialized {
     use InitTrait;
-    public function __construct(array $config = [])
-    {
+    public function __construct(array $config = []) {
         $this->initialize($config);
     }
 
-    public static function turnInto(mixed $input): mixed
-    {
-        $result = null;
-
+    public static function turnInto(mixed $input): mixed {
         $class_name = null;
-        $args = [];
+        $config     = [];
         if (is_string($input)) {
             $class_name = $input;
         } elseif (is_array($input) && count($input) > 0) {
             $class_name = isset($input['class']) ? $input['class'] : null;
-            $args = isset($input['config']) ? $input['config'] : array_diff_key($input, array_fill_keys(['class', 'config'], 'empty'));
+            $config     = isset($input['config']) ? $input['config'] : array_diff_key($input, array_fill_keys(['class', 'config'], 'empty'));
         }
 
-        if (!class_exists((string)$class_name)) {
+        if (! class_exists((string) $class_name)) {
             throw new RuntimeException(sprintf('%s: failed retrieving class name "%s" via mixed "%s"; class does not exist', __METHOD__, StringHelper::className($class_name, true), gettype($class_name)));
         }
 
-        if (count($args) > 0) {
-            $result = new $class_name($args);
-        } else {
-            $result = new $class_name();
+        $reflection = new ReflectionClass($class_name);
+        if (empty($config)) {
+            return $reflection->newInstance();
         }
 
-        return $result;
+        $constructor = $reflection->getConstructor();
+        if ($constructor === null) {
+            return $reflection->newInstance();
+        }
+
+        $parameters = $constructor->getParameters();
+
+        if (count($parameters) === 1 && $parameters[0]->getType() && $parameters[0]->getType()->getName() === 'array') {
+            // Constructor expects an array
+            return $reflection->newInstance($config);
+        }
+
+        $args = [];
+        foreach ($parameters as $parameter) {
+            $name = $parameter->getName();
+            if (array_key_exists($name, $config)) {
+                $args[] = $config[$name];
+            } elseif ($parameter->isDefaultValueAvailable()) {
+                $args[] = $parameter->getDefaultValue();
+            } else {
+                throw new RuntimeException(sprintf('Missing required parameter "%s" for "%s"', $name, $class_name));
+            }
+        }
+
+        return $reflection->newInstanceArgs($args);
     }
 }
 /** End of AutoInitialized **/
